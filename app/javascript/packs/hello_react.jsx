a26d4817fb5cd4bbd05b8920f02ae92c2b8eb3bd
Saadat Atta <muhammad.saadat@phaedrasolutions.com>
2022-02-03 17:05:23 +0500

    Dummy component with polaris added

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -1,26 +1,58 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import {Page, Card, Button, Thumbnail, AppProvider} from '@shopify/polaris';
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+const Hello = props => {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        async function fetchProducts(){
+            setTimeout(()=>{
+                var headers = new Headers({"Authorization": "Bearer " + window.sessionToken});
+                return fetch("/products", {headers})
+                    .then(response => response.json())
+                    .then(data => {
+                        var products = data.products;
 
-Hello.defaultProps = {
-  name: 'David'
+                        if (products === undefined || products.length == 0) {
+                            setProducts([])
+                        } else {
+                            setProducts(products)
+                        }
+                    });
+            },1000)
+
+        }
+        fetchProducts()
+
+
+    }, [])
+    return (
+        <Page title="Products">
+            {products && products.map((product, index) => (
+                <Card key={index}
+                      title={product.title}
+                      primaryFooterAction={{
+                          content: 'View',
+                          url: 'https://${shop_session.url}/admin/products/${product.id}',
+                      }}
+                      sectioned
+                >
+                    <Thumbnail
+                        source={((product.images !== null) ? product.images[0].src : 'https://images-na.ssl-images-amazon.com/images/I/91JKyhY%2BtFL._SL1500_.jpg')}
+                        alt={product.title}
+                        size="large"
+                    />
+
+                </Card>
+            ))}
+        </Page>
+    )
 }
 
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
+// Render component with data  
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
-  )
+    const node = document.getElementById('hello-react')
+    const data = JSON.parse(node.getAttribute('data'))
+    ReactDOM.render(<AppProvider><Hello {...data} /></AppProvider>, node)
 })
